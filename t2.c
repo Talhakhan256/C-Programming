6a257b81d4af94b312c9150cc142784b5c75c54c
Talhakhan256 <tk96038@eng.rizvi.edu.in>
2025-10-16 10:19:45 +0530

    Add files via upload

---
+++ t2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+    //TALHA KHAN
+    int num;
+    printf("enter a number:");
+    scanf("%d,&num");
+
+    if(num%2==0)
+       printf("%d is even\n",num);
+    else
+       printf("%d is odd\n",num);
+    return 0;
+    }
