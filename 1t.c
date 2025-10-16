9aca0ea5ddf08a625d7e84dd0e453eee4ccf210b
Talhakhan256 <tk96038@eng.rizvi.edu.in>
2025-10-16 10:10:12 +0530

    Add files via upload

---
+++ 1t.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+
+    int marks=34;
+    if (marks>=80){
+    printf("grade A+");}
+    else if(marks>=60){
+    printf("grade A");}
+    else if("marks>=45"){
+    printf("grade B");}
+    else {
+    printf("fail");}
+
+    return 0;
+    }
