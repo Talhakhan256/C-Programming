f88dfa9f41dd86e652c47bd44bbce7a97c74aeeb
Talhakhan256 <tk96038@eng.rizvi.edu.in>
2025-10-16 09:57:55 +0530

    Add files via upload

---
+++ exp1kt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+
+   /*
+   name:Talha Khan
+   age:18
+   uin:251P006
+   division:C
+   class:computers
+   */
+
+   printf("name:Talha Khan\n");
+   printf("age:18 years\n");
+   printf("uin:251P006\n");
+   printf("division:C\n");
+   printf("class:computers\n");
+
+
+   return 0;
+   }
