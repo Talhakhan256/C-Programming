220ba3effff11c5a8f546158dcf2a5b66b280855
Talhakhan256 <tk96038@eng.rizvi.edu.in>
2025-12-01 12:18:49 +0530

    Add files via upload

---
+++ tkexp8a.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int main() {
+    //TALHA KHAN
+    char str[100];
+    int i = 0;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    while (str[i] != '\0') {
+        i++;
+    }
+
+    printf("Length of the string = %d", i);
+
+    return 0;
+}
+
