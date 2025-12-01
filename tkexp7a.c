e6ba6968d0c5c2190fbe28e49d96c2f8f57d88f5
Talhakhan256 <tk96038@eng.rizvi.edu.in>
2025-12-01 12:17:32 +0530

    Add files via upload

---
+++ tkexp7a.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+int main() {
+    int a[100], n, i, max;
+    //TALHA KHAN
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    max = a[0];
+    for (i = 1; i < n; i++) {
+        if (a[i] > max)
+            max = a[i];
+    }
+
+    printf("Largest element = %d", max);
+
+    return 0;
+}
+
