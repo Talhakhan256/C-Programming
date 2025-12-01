03c3c70034e6e51ec908788eb59db970d04630a1
Talhakhan256 <tk96038@eng.rizvi.edu.in>
2025-12-01 12:16:06 +0530

    Add files via upload

---
+++ tkexp4.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+
+int main() {
+    int start, end, i, j, flag;
+    //TALHA KHAN
+    printf("Enter the start and end of the range: ");
+    scanf("%d %d", &start, &end);
+
+    printf("Prime numbers between %d and %d are:\n", start, end);
+
+    for (i = start; i <= end; i++) {
+        if (i <= 1) continue;
+
+        flag = 1;
+
+        for (j = 2; j <= i / 2; j++) {
+            if (i % j == 0) {
+                flag = 0;
+                break;
+            }
+        }
+
+        if (flag == 1) {
+            printf("%d ", i);
+        }
+    }
+
+    return 0;
+}
